6b944d30aa39c951c3eef5fc62a252ba4ccaaf39
Hasan Kurtoğlu <hasankurtoglu6714@gmail.com>
2023-12-08 20:27:50 +0300

    fonksiyon.py eklendi

---
+++ git_egitim/fonksiyon.py
@@ -0,0 +1,5 @@
+def sayilari_topla(a,b):
+    return a+b
+
+def sayilari_cikar(a,b):
+    return a-b
---
+++ git_egitim/fonksiyon2.py
@@ -0,0 +1,5 @@
+def sayilari_carp(a,b):
+    return a*b
+
+def sayilari_bol(a,b):
+    return a/b
